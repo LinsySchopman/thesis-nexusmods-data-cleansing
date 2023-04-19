786f4cd54d19975c6b9a2f699c7b901974625e00
LinsySchopman <118113015+LinsySchopman@users.noreply.github.com>
2023-04-19 15:05:12 +0200

    Converts epoch to datetime (3)

--- convert_datatime.py
+++ convert_datatime.py
@@ -1,3 +1,4 @@
+# Import libraries
 import csv
 import datetime
 
@@ -20,35 +21,3 @@ with open(r'K:\Thesis Data\Sample B\Nexus Mods\site-stats.csv', 'r') as infile, 
 
         # Write the row to the output file, including the datetime value
         writer.writerow(row + [dt])
-
-
-
-
-# Splits date and time into separate columns
-import csv
-import datetime
-
-# Open the input and output CSV files
-with open(r'K:\Thesis Data\Sample B\Nexus Mods\site-stats.csv', 'r') as infile, \
-     open(r'K:\Thesis Data\Sample B\Nexus Mods\site-stats-split.csv', 'w', newline='') as outfile:
-
-    reader = csv.reader(infile)
-    writer = csv.writer(outfile)
-
-    # Write the header row to the output file
-    header = next(reader)
-    writer.writerow(header + ['date', 'time'])
-
-    # Loop over the rows in the input file
-    for row in reader:
-        # Convert epoch time to datetime
-        epoch_time = int(row[0])
-        dt = datetime.datetime.fromtimestamp(epoch_time)
-
-        # Split datetime into date and time
-        date_str = dt.strftime('%Y-%m-%d')
-        time_str = dt.strftime('%H:%M:%S')
-
-        # Write the row to the output file, including the date and time columns
-        writer.writerow(row + [date_str, time_str])
-
