1952b20d910ad2f7b66fe2998dc5ff949578dad9
LinsySchopman <118113015+LinsySchopman@users.noreply.github.com>
2023-04-19 14:57:54 +0200

    adds nexus_id identifier (2)

--- add_nexusid.py
+++ add_nexusid.py
@@ -1,15 +1,27 @@
+# Import libraries
 import os
 import pandas as pd
 
+# Set the filepath
 folder_path = r'K:\Thesis Data\Sample B\Nexus Mods\new_files
 
-# loop through all files in the folder
+# Loop through all files in the folder
 for file_name in os.listdir(folder_path):
     if file_name.endswith('.csv'):
         file_path = os.path.join(folder_path, file_name)
-        nexus_id = file_name[:-4] # remove ".csv" from file name
+        
+        # Remove ".csv" from file name
+        nexus_id = file_name[:-4] 
         df = pd.read_csv(file_path)
-        df.columns = ['date', 'new_files', 'new_images', 'new_videos'] # rename existing columns
-        df.insert(0, 'nexus_id', nexus_id) # insert "nexus_id" column at the beginning of the DataFrame
-        df = df[['nexus_id', 'date', 'new_files']] # reorder columns
-        df.to_csv(file_path, index=False) # overwrite the original file with the modified DataFrame
+        
+        # Rename existing columns
+        df.columns = ['date', 'new_files', 'new_images', 'new_videos'] 
+        
+        # Insert "nexus_id" column at the beginning of the DataFrame
+        df.insert(0, 'nexus_id', nexus_id) 
+        
+        # Reorder columns
+        df = df[['nexus_id', 'date', 'new_files']] 
+        
+        # Overwrite the original file with the modified DataFrame
+        df.to_csv(file_path, index=False) #
